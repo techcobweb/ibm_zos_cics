52654d27bfbabf21a8ec3700ab191fc664eb42fd
yourfuwa2015 <yourfuwa@163.com>
2020-06-30 19:02:38 +0800

    fix module name

--- plugins/modules/cics_dfhcsdup.py
+++ plugins/modules/cics_dfhcsdup.py
@@ -12,7 +12,7 @@ ANSIBLE_METADATA = {'metadata_version': '1.1',
                     'supported_by': 'community'}
 
 DOCUMENTATION = r"""
-module: cics_dfhcsdup_new
+module: cics_dfhcsdup
 author:
     - "Zhao Lu (@zlbjlu)"
 short_description: CICS system definition utility program(DFHCSDUP)
